69879840e067e89d1126e87442d71def3850a506
Penghao Shi <penghshi@tesla.com>
2023-06-18 20:28:53 +0800

    010-deploy-a-linux-vm-with-cloud-init-yaml

---
+++ 010-provisioners/provider.tf
@@ -0,0 +1,22 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~>4.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
---
+++ 010-provisioners/terraform.tfvars
@@ -0,0 +1,2 @@
+resource_group_name_prefix = "rg"
+resource_group_location    = "westus2"
